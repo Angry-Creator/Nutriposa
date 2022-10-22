e571ac392ac28b7741684066997c918ff472e6d4
Angry-Creator <nwankwochidera252565@gmail.com>
2022-10-22 17:46:41 +0100

    Fixing both responsiveness, adding text carousel and also fixed navigation

--- assets/js/services-theme-changer.js
+++ assets/js/services-theme-changer.js
@@ -1,10 +1,16 @@
 //When the night toggler btn is clicked dark mode becomes active
+
 nightToggler.onclick = () =>{
     nightToggler.classList.toggle("bi-toggle-on");
+    //dark theme
     if(nightToggler.classList.contains("bi-toggle-on")){
         nav.style.backgroundColor = "var(--dark-theme-bg-color)";
-    }else{
+        saveState("dark")
+    }
+    //light theme
+    else{
         nav.style.backgroundColor = "var(--light-theme-bg-color)";
+        saveState("light")
     }
 }
 
