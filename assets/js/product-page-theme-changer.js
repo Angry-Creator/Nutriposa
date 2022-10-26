34aa9aaa5d935887be2bbd4ff8a7966389569e69
Angry-Creator <nwankwochidera252565@gmail.com>
2022-10-26 15:19:43 +0100

    Working on the Product Page

--- assets/js/product-page-theme-changer.js
+++ assets/js/product-page-theme-changer.js
@@ -1,8 +1,7 @@
 //When the night toggler btn is clicked dark mode becomes active
 
 //Since you're in dark theme it changes black to white and white theme changes white to black
-nightToggler.onclick = () =>{
-    const textCarousel = document.getElementById("carousel-text");
+nightToggler.onclick = () => {
     const changeBlackToWhite = document.querySelectorAll(".changeBlackToWhite");
     const changeWhiteToBlack = document.querySelectorAll(".changeWhiteToBlack");
     const btnThemeControl = document.querySelectorAll(".btn-theme-control");
@@ -10,15 +9,13 @@ nightToggler.onclick = () =>{
 
     nightToggler.classList.toggle("bi-toggle-on");
     //dark theme
-    if(nightToggler.classList.contains("bi-toggle-on")){
+    if (nightToggler.classList.contains("bi-toggle-on")) {
         nav.style.backgroundColor = "var(--dark-color)";
         document.body.style.backgroundColor = "var(--dark-color)";
-        textCarousel.style.backgroundColor = "var(--dark-color)";
-        textCarousel.style.color = "var(--light-color)";
         serviceContact.style.backgroundColor = "var(--light-color)";
         serviceContact.style.color = "var(--dark-color)";
 
-        changeBlackToWhite.forEach((element)=>{
+        changeBlackToWhite.forEach((element) => {
             element.classList.toggle("changeBlackToWhite");
             element.classList.toggle("changeWhiteToBlack");
         });
@@ -31,21 +28,19 @@ nightToggler.onclick = () =>{
         saveState("dark");
     }
     //light theme
-    else{
+    else {
         nav.style.backgroundColor = "var(--light-header-color)";
         document.body.style.backgroundColor = "var(--light-color)";
-        textCarousel.style.backgroundColor = "var(--light-text-carousel-color)";
-        textCarousel.style.color = "var(--light-color)";
         serviceContact.style.backgroundColor = "var(--light-text-carousel-color)";
         serviceContact.style.color = "var(--light-color)";
 
-        changeWhiteToBlack.forEach((element)=>{
+        changeWhiteToBlack.forEach((element) => {
             element.classList.toggle("changeWhiteToBlack");
             element.classList.toggle("changeBlackToWhite");
         });
 
         btnThemeControl.forEach((element)=>{
-            element.style.backgroundColor = "var(--light-text-carousel-color)";
+            element.style.backgroundColor = "var(--light-header-color)";
             element.style.color = "var(--light-color)";
         });
 
